b4e0df2c0192f56880a772299a01d06713a00a7d
carlosnunesjr <carlosnunes@gmail.com>
2023-01-12 16:47:18 -0300

    add readme

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -3,7 +3,7 @@ import Prismic from "@prismicio/client";
 export const repositoryName = "ignews";
 
 export function getPrismicClient(req?: unknown) {
-  const prismic = Prismic.client(process.env.PRISMIC_END_POINT, {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req
   });
